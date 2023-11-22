f88249d2176db2d0b36a32765d322f00954e1a0b
Ernest <gbaboh.ernest@gmail.com>
2023-11-22 13:59:31 +0000

    modifies the console.py function to properly allow for object creation with given parameters, handle underscores and double quotes in argument

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}' \
+                    if pline[0] == '{' and pline[-1] == '}' \
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,12 +115,12 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        # split arguments by space to get cls name and parameters
+        args_list = args.split()
+        if not args_list:  # check if cls name is missing
             print("** class name missing **")
             return
 
-        # split arguments into cls name and parameters
-        args_list = args.split(" ")
         cls_name = args_list[0]  # save class name
 
         # check if class exists in HBNBCommand classes dict
@@ -128,35 +128,33 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
 
-        # pop cls_name from arguments list
-        args_list.pop(0)
-        params_dict = {}  # empty dict to store parameters
+        # create a new instance of the class
+        new_instance = HBNBCommand.classes[cls_name]()
 
-        # parse & populate parameters dict
-        for params in args_list:
-            # split the parameters into key and value
-            key, value = params.split("=")
+        # process parameter if args are more
+        if len(args_list) > 1:
+            # iterate over each arg, split into key and value pairs
+            for arg in args_list[1:]:
+                key, value = arg.split("=")
 
-            # handle string values with double quotes
-            if value.startswith('"') and value.endswith('"'):
-                value = value[1:-1].replace('_', ' ').replace('\\"', '"')
+                # replace underscores with space in the value
+                value = value.replace('_', ' ')
 
-            try:  # try converting to int, float, | keep string based on format
-                if '.' in value:
-                    value = float(value)
-                else:
-                    value = int(value)
-            except ValueError:
-                pass  # if fails, keep value as a string
+                # trim quotes from key & value
+                key = key.strip("'\"")
+                value = value.strip("'\"")
 
-            # add key-value pairs to the params dict
-            params_dict[key] = value
+                # set attr in the instance
+                setattr(new_instance, key, value)
 
-        # create an instance of specified class with parsed parameters
-        new_instance = HBNBCommand.classes[cls_name](**params_dict)
-        storage.save()  # save instance & print its ID
-        print(new_instance.id)
-        storage.save()
+            # save & print id of the new instance after setting attributes
+            new_instance.save()
+            print(new_instance.id)
+
+        else:
+            # save instance if no additional args are provided
+            new_instance.save()
+            print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -304,7 +302,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -312,10 +310,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
