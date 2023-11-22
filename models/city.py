e3265e642b52937ec46a655995e5cccf624b8623
Ernest <gbaboh.ernest@gmail.com>
2023-11-22 22:58:58 +0000

    update City, inherits from BaseModel & Base, add class attribute: cities, name &state_id with a foreign key relationship state.id

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel
+from models.base_model import Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    state_id = Column("state_id", String(60), )
+    name = Column("name", String(128), ForeignKey('states.id'), nullable=False)
