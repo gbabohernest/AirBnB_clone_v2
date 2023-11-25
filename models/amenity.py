16ec9e36d930e49d19b3840d609000e8f9415b56
Ernest <gbaboh.ernest@gmail.com>
2023-11-25 18:14:47 +0000

    update Amenity: create class attributes create many-to-many relationship between Place and Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, Table
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class to store amenity information"""
+
+    __tablename__ = "amenities"
+
+    name = Column("name", String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity,
+                                   backref='amenities')
