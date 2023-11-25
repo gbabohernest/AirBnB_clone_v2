612a9376dae4187e68a7d110c9771a88aa9a21fa
Ernest <gbaboh.ernest@gmail.com>
2023-11-25 18:25:04 +0000

    update Place: add SQLAlchemy Table instance "place_amenity" for creating Many-toMany relationship between Place and Amenity, also add an attribute amenities for DBStorage that represent a relationship with class Amenity but also secondary to place_amenity, create a getter and setter attribute for when it is FileStorage that return a list Amenity instances based on the aminity_ids and also handles adding Amenity.id to the attribute amenity_ids when the object been passed to the setter attribute is an instance of the Amenity class, otherwise do nothing

--- models/place.py
+++ models/place.py
@@ -2,12 +2,27 @@
 """ Place Module for HBNB project """
 
 from os import getenv
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
 
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+
+place_amenity = Table('place_amenity',
+                      Base.metadata,
+                      Column('place_id',
+                             String(60),
+                             ForeignKey('places.id'),
+                             nullable=False,
+                             primary_key=True),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey('amenities.id'),
+                             nullable=False,
+                             primary_key=True))
 
 
 class Place(BaseModel, Base):
@@ -29,6 +44,9 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", cascade="all, delete",
                                backref="place")
 
+    amenities = relationship("Amenity", secondary=place_amenity,
+                             viewonly=False)
+
     else:
         # For FileStorage
         @property
@@ -38,3 +56,27 @@ class Place(BaseModel, Base):
             """
             return [review for review in models.storage.all(Review).all()
                     if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Getter attribute for amenities in FileStorage.
+            Return: A list of Amenities instances based on
+            the attribute amenity_ids that contains all Amenity.id
+            linked to the Place.
+            """
+
+            return [amenity for amenity in models.storage.all(Amenity).values()
+                    if amenity.id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, amenity_obj):
+            """Setter attribute for amenities in FileStorage.
+            Handles append method for adding an Amenity.id to the
+            attribute amenity_id.
+            """
+            if isinstance(amenity_obj, Amenity):
+                self.amenity_ids.append(amenity_obj.id)
+                # ignore if input is not an amenity object/instance
+
+        # attribute to store amenity ids
+        amenity_ids = []
