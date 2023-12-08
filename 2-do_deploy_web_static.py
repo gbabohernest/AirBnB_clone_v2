f407df2ffafbfd4dc3d46418974573ff99f820fd
Ernest <gbaboh.ernest@gmail.com>
2023-12-08 02:57:52 +0000

    deploy archive!!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,50 +2,48 @@
 """This module defines a Fabric script to distribute
    archived web_static to web servers
 """
-
-from os.path import exists
-from fabric.api import local, run, env, put
+import os.path
+from fabric.api import env, put, run
 
 env.hosts = ['54.146.86.193', '54.209.169.142']
-env.user = 'ubuntu'
-env.key_filename = ['/home/vagrant/.ssh/id_rsa']
 
 
 def do_deploy(archive_path):
-    """
-      Distributes archived web_static from do_pack() to web servers
+    """Distributes archived web_static from do_pack() to web servers
 
-      Args:
+       Args:
           archive_path (str): Path to the archive file.
 
-      Returns:
+       Returns:
           bool: True if deployment succeeds, False otherwise.
-      """
-
-    if not exists(archive_path):
+    """
+    if not os.path.isfile(archive_path):
         return False
 
-    try:
-        # upload the archive to /tmp/ directory on the remote servers
-        put(archive_path, "/tmp/")
-        # Extract the contents to /data/web_static/releases/
-        filename = archive_path.split('/')[-1]
-        directory_name = "/data/web_static/releases/{}".format(filename
-                                                               .split('.')[0])
-        run("sudo mkdir -p {}".format(directory_name))
-        run("sudo tar -xzf /tmp/{} -C {}".format(filename, directory_name))
+    filename = os.path.basename(archive_path)
+    name = filename.split(".")[0]
 
-        # Delete the archive from the web server
-        run("sudo rm /tmp/{}".format(filename))
-        # Delete symbolic link
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create new symbolic link
-        run("sudo ln -s {} /data/web_static/current".format(directory_name))
-        print("New version deployed!")
-
-        return True
-
-    except Exception as e:
-        print("Deployment failed", str(e))
+    if put(archive_path, "/tmp/").failed:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            filename, name)).failed:
+        return False
+    if run("rm /tmp/{}".format(filename)).failed:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(
+            name)).failed:
+        return False
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ "
+           "/data/web_static/current".format(name)).failed:
+        return False
+
+    return True
