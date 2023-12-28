703b8d20e56c30a02daa3c70b6331c217fb77547
Ernest <gbaboh.ernest@gmail.com>
2023-12-28 23:33:08 +0000

    using two routes, I am displaying Python is cool if user don't provide value for text variable, if text variable value is provided, display Python along with the value of the text variable replacing any underscore within the value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application
+listening on 0.0.0.0, port 5000
+
+Routes:
+        /: display 'Hello HBNB!'
+        /hbnb: display 'HBNB'
+        /c/<text>: display “C ” followed by the value of
+        the text variable (replace underscore _ symbols
+        with a space )
+        /python/<text>: display “Python ”, followed by the
+        value of the text variable (replace underscore _ symbols
+        with a space )
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display Hello HBNB! in the browser"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display HBNB in the browser"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Display C followed by the value of text variable.
+    Replaced underscore _ symbols with a space
+    """
+    replace_text_with_spaces = text.replace('_', ' ')
+    return 'C {}'.format(replace_text_with_spaces)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_route(text='is cool'):
+    replace_text_with_spaces = text.replace('_', ' ')
+    return 'Python {}'.format(replace_text_with_spaces)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
