0dd2e03234381ec82cee9e2d3a03c2f4fd23bbe9
Ernest <gbaboh.ernest@gmail.com>
2023-12-30 20:01:56 +0000

    typecast the n arg to integer

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -47,12 +47,11 @@ def python_route(text='is cool'):
     return 'Python {}'.format(replace_text_with_spaces)
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     """Display n is a number, only if n is an integer"""
 
-    if n.isdigit():
-        return '{} is a number'.format(n)
+    return '{} is a number'.format(n)
 
 
 if __name__ == "__main__":
