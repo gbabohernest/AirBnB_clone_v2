48b57f416190d12d018be4f2b58aa934729b2325
Ernest <gbaboh.ernest@gmail.com>
2023-12-31 22:38:41 +0000

    fetch state objects

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -25,7 +25,7 @@ def states_list():
     """Fetch all states objects from storage engine
     and sort them by name and display rendered HTML page
     """
-    states = storage.all(State).values()
+    states = storage.all("State").values()
     sorted_states = sorted(states, key=attrgetter('name'))
     return render_template('7-states_list.html', states=sorted_states)
 
