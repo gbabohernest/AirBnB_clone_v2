8cfbcd3ff68a041f3020b4b6c9cd208a8a4e73ae
Ernest <gbaboh.ernest@gmail.com>
2023-12-31 23:38:28 +0000

    State and state

---
+++ web_flask/9-states.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application
+listening on 0.0.0.0, port 5000
+Uses storage to fetch data from storage engine.
+A teardown method to remove the current SQLAlchemy session
+Display HTML page with State and list of all state objects
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(e):
+    """Remove SQLAlchemy session after each request"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Fetch all states objects from storage engine
+    and sort them by name and display rendered HTML page
+    """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Fetch all states objects from storage engine
+       and sort them by name and display rendered HTML page
+       uses cities relationship or getter method to list cities by state
+       """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id=None):
+    """Fetch all states objects from storage engine
+          and sort them by name and display rendered HTML page
+          uses cities relationship or getter method to list cities by state
+    """
+    states = storage.all(State).values()
+
+    if id is None:
+        sorted_states = sorted(states, key=lambda state: state.name)
+        return render_template('9-states.html', states=sorted_states, id=id)
+
+    for state in states:
+        if state.id == id:
+            return render_template('9-states.html', states=[state], id=id)
+
+    return render_template('9-states.html', states=None, id=id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
